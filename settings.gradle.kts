50de725d1277bd31ac691fb11455e9dff5b351a7
tilsley <nathantilsley@gmail.com>
2018-12-24 18:52:30 +0000

    add docker and semantic versioning

---
+++ settings.gradle.kts
@@ -0,0 +1,12 @@
+rootProject.name = "demo"
+
+buildscript {
+    repositories {
+        maven("https://plugins.gradle.org/m2/")
+    }
+    dependencies {
+        classpath("gradle.plugin.net.vivin:gradle-semantic-build-versioning:4.0.0")
+    }
+}
+
+apply(plugin = "net.vivin.gradle-semantic-build-versioning")
